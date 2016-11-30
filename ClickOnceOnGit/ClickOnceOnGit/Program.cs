a5f8287c638b0a319ed4dea3b2c474f5b2745235
Mofaggol Hoshen <summoncse@gmail.com>
2016-11-30 13:34:53 +0100

    First

---
+++ ClickOnceOnGit/ClickOnceOnGit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickOnceOnGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("String to print:)");
+            Console.WriteLine($"{Console.ReadLine()}{Environment.NewLine}Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
